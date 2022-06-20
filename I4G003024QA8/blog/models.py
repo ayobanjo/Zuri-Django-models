06a807e96cf26fb5d5c9d117021f81f7d0c67653
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-06-20 20:31:59 +0100

    m: created post model and migrated

--- I4G003024QA8/blog/models.py
+++ I4G003024QA8/blog/models.py
@@ -1,3 +1,12 @@
+from typing import Text
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.GenericIPAddressField()
+    Created_date = models.DateTimeField(auto_now_add= True)
+    Published_date = models.DateTimeField()
+
